381317166288f91d4a7f6acf73346e576ff6f7f0
Frederik Creemers <frederikcreemers@gmail.com>
2023-12-05 16:03:52 +0100

    feat: add useUnreads

--- example/App.tsx
+++ example/App.tsx
@@ -1,8 +1,8 @@
 import "./App.css";
 
-import { Session, Chatbox } from "../lib/main";
+import { Session, Chatbox, useUnreads } from "../lib/main";
 import Talk from "talkjs";
-import { ChangeEvent, useCallback, useMemo, useRef, useState } from "react";
+import { ChangeEvent, useCallback, useMemo, useRef, useState, ReactElement } from "react";
 
 const convIds = ["talk-react-94872948u429843", "talk-react-194872948u429843"];
 const users = [
@@ -151,6 +151,7 @@ function App() {
           {...(blur ? { onBlur } : {})}
           style={{ width: 500, height: 600 }}
         />
+        <UnreadsDisplay />
       </Session>
       <button onClick={otherMe}>switch user (new session)</button>
       <br />
@@ -193,6 +194,28 @@ function App() {
       </fieldset>
     </>
   );
+}
+
+function UnreadsDisplay() {
+  const unreads = useUnreads();
+  let content: ReactElement | null = null;
+
+  if (unreads === undefined) {
+    content = <p>unreads is undefined (no session)</p>;
+  } else if (unreads.length === 0) {
+    content = <p>No unread messages</p>
+  } else {
+    content = <ul>
+      {unreads.map(u => {
+        return <li key={u.conversation.id}>{u.conversation.id} - {u.lastMessage.sender?.name || "system"}: {u.lastMessage.body}</li>
+      })}
+    </ul>
+  }
+
+  return <details>
+    <summary><strong>Unreads rendered with useUnreads</strong></summary>
+    {content}
+  </details>
 }
 
 export default App;
--- lib/main.tsx
+++ lib/main.tsx
@@ -3,4 +3,4 @@ export { Session } from "./Session";
 export { Chatbox } from "./ui/Chatbox";
 export { Inbox } from "./ui/Inbox";
 export { Popup } from "./ui/Popup";
-export { useSession } from "./SessionContext";
+export { useSession, useUnreads } from "./SessionContext";
