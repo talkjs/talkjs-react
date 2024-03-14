801d437f3de150f1398791b8a806b732c563e9fd
Frederik Creemers <frederikcreemers@gmail.com>
2024-03-14 11:29:57 +0100

    Remove popup tester from example code

--- example/main.tsx
+++ example/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import App from "./App";
+import App from "./App";
 import "./index.css";
-import PopupExample from "./Popup";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PopupExample />
+    <App />
   </React.StrictMode>,
 );
